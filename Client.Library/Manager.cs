52287298a7aeb551c502e39b52100e28440d007b
Haydar Kaan Güzelimdağ <guzelimdag.hk@gmail.com>
2021-12-05 15:42:02 +0300

    Add gRPC Client library
    Client library to use gRPC client separately from the service. This approachment was preferred to work on WPF App with the client.

---
+++ Client.Library/Manager.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+using SrvClient = GameGrpcServiceV1.TicTacToeSrv.TicTacToeSrvClient;
+
+namespace Client.Library
+{
+    public class Manager
+    {
+        DateTime Timeout { get { return DateTime.UtcNow.AddSeconds(2); } }
+
+        Channel? _channel;
+        SrvClient? _client;
+
+        public  async Task<SrvClient?> SetConnection(string host, int port)
+        {
+            try
+            {
+                try
+                {
+                    _channel = new Channel(host, port, ChannelCredentials.Insecure);
+                    await _channel.ConnectAsync(Timeout);
+                }
+                catch (Exception)
+                {
+                    _channel = new Channel("127.0.0.1", port, ChannelCredentials.Insecure);
+                    await _channel.ConnectAsync(Timeout);
+                }
+                
+                _client = new SrvClient(_channel);
+                return _client;
+            }
+            catch (Exception)
+            { return null; }
+        }
+    }
+}
